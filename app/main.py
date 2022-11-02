26dc1c4de97551a4915837e380d6cd35a6de384f
Mateus Souza <mateus_msouza@outlook.com>
2022-11-02 15:23:19 -0300

    fix again

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from services.get_extract import get_extract
 from connections.send_message import send_message
 
 if __name__ == "__main__":
-    #create_debt()
+    create_debt()
     message = get_extract()
-    print(message)
-    #send_message(message=message)
+    send_message(message=message)
